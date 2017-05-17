1688189421ff4a90d7a11fd1768ca3be480d07c2
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-16 23:32:53 -0300

    Removing "axios" test

--- test/abc.test.js
+++ test/abc.test.js
@@ -3,17 +3,17 @@ var axios = require('axios');
 var fetch = require('whatwg-fetch').fetch;
 var request = require('request');
 
-describe('Axios - Trying to simulate error occurring only on travis.', function () {
-    it('Should make a request', function (done) {
+// describe('Axios - Trying to simulate error occurring only on travis.', function () {
+//     it('Should make a request', function (done) {
 
-        axios.get('http://www.google.com')
-            .then(function (response) {
-                expect(response.data).to.contains('<html');
-                done();
-            })
-            .catch(done);
-    });
-});
+//         axios.get('http://www.google.com')
+//             .then(function (response) {
+//                 expect(response.data).to.contains('<html');
+//                 done();
+//             })
+//             .catch(done);
+//     });
+// });
 
 describe('Request - Trying to simulate error occurring only on travis.', function () {
     it('Should make a request', function (done) {        
