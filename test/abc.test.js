d5764b9615573eb7063e7c9008ffd93be5cb1213
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-05-16 23:31:23 -0300

    Testing using request

--- test/abc.test.js
+++ test/abc.test.js
@@ -1,7 +1,9 @@
 var expect = require('chai').expect;
 var axios = require('axios');
+var fetch = require('whatwg-fetch').fetch;
+var request = require('request');
 
-describe('Trying to simulate error occurring only on travis.', function () {
+describe('Axios - Trying to simulate error occurring only on travis.', function () {
     it('Should make a request', function (done) {
 
         axios.get('http://www.google.com')
@@ -9,6 +11,15 @@ describe('Trying to simulate error occurring only on travis.', function () {
                 expect(response.data).to.contains('<html');
                 done();
             })
-            .catch(done);        
+            .catch(done);
+    });
+});
+
+describe('Request - Trying to simulate error occurring only on travis.', function () {
+    it('Should make a request', function (done) {        
+        request('http://www.google.com', function (error, response, body) {
+            expect(body).to.contains('<html');
+            done();
+        });
     });
 });
